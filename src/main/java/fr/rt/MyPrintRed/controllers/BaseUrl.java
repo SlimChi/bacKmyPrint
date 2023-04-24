592a31226a4b77939b44791586c15ae370937f96
Redouane <redouane.takdjerad@gmail.com>
2023-04-24 15:20:29 +0200

    ajout des urls de base pour hateoas

--- src/main/java/fr/rt/MyPrintRed/controllers/BaseUrl.java
+++ src/main/java/fr/rt/MyPrintRed/controllers/BaseUrl.java
@@ -2,10 +2,27 @@ package fr.rt.MyPrintRed.controllers;
 
 public class BaseUrl {
 
-    final static String FRONT_BASE_URL ="http://localhost:3000";
-    final static String UTILISATEUR_BASE_URL ="http://localhost:8080/adresseutilisateur";
-    final static String ADRESSE_BASE_URL = "http://localhost:8080/adresses";
-    final static String ADRESSE_UTILISATEUR_BASE_URL ="http://localhost:8080/adresseutilisateur";
+    private static String URL = "http://localhost:8080/";
+    final static String FRONT_BASE_URL ="http://localhost:4200";
+    final static String UTILISATEUR_BASE_URL = URL+"adresseutilisateur";
+    final static String ADRESSE_BASE_URL = URL+"adresses";
+    final static String ADRESSE_UTILISATEUR_BASE_URL =URL+"adresseutilisateur";
 
-    final static String COMMANDE_BASE_URL ="http://localhost:8080/commandes";
+    final static String COMMANDE_BASE_URL =URL+"commandes";
+
+    final static String LIGNE_COMMANDE_BASE_URL = URL+"lignecommandes";
+
+    final static String STATUS_BASE_URL = URL + "statuses";
+
+    final static String OPTION_BASE_URL = URL +"options";
+
+    final static String OPTION_CATEGORIE_BASE_URL = URL +"optioncategories";
+
+    final static String OPTION_LIGNE_COMMANDE_BASE_URL = URL + "optionlignecommandes";
+
+    final static String TYPE_OPTION_BASE_URL = URL + "typeoptions";
+
+    final static String INTERVENIR_BASE_URL = URL + "intervenirs";
+
+    final static String FICHIER_BASE_URL = URL + "fichiers";
 }
